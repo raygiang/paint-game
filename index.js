344057106c97e069acc2fd0685891f25251923b8
Ray <raymondgiang@gmail.com>
2019-02-18 21:24:17 -0500

    changed assets, shows end game results

--- index.js
+++ index.js
@@ -7,69 +7,32 @@ var app = express();
 var server = http.createServer(app);
 var io = socketIO.listen(server);
 
-var yoda = {
-    x: 40,
-    y: 30,
-    width: 30,
-    height: 50,
-    spriteSheetY: 0,
-    spriteSheetLocations: [0, 145, 98, 48],
-    currFrame: 0,
-    animationFrames: [0, 33, 66, 99],
-    isMoving: false,
-    paintColour: "lightgreen",
-    imgNum: 0
-};
-
-var luke = {
-    x: 40,
-    y: 510,
-    width: 30,
-    height: 50,
-    spriteSheetY: 0,
-    spriteSheetLocations: [0, 145, 98, 48],
-    currFrame: 0,
-    animationFrames: [0, 33, 66, 99],
-    isMoving: false,
-    paintColour: "lightgreen",
-    imgNum: 1
-};
-
-var ironman = {
-    x: 530,
-    y: 30,
-    width: 30,
-    height: 50,
-    spriteSheetY: 0,
-    spriteSheetLocations: [0, 145, 95, 48],
-    currFrame: 0,
-    animationFrames: [0, 33, 66, 99],
-    isMoving: false,
-    paintColour: "red",
-    imgNum: 2
-};
-
-var cap = {
-    x: 530,
-    y: 510,
-    width: 30,
-    height: 46,
-    spriteSheetY: 0,
-    spriteSheetLocations: [0, 145, 96, 48],
-    currFrame: 0,
-    animationFrames: [0, 33, 66, 99],
-    isMoving: false,
-    paintColour: "red",
-    imgNum: 3
-};
-
-var playerChars = {};
-
 app.use(express.static(path.join(__dirname + "/public")));
 server.listen(process.env.PORT || 9002, '0.0.0.0');
 console.log("Server running on localhost:9002");
 
+function Player(startX, startY, paintCol, imgNum) {
+    this.x = startX;
+    this.y = startY;
+    this.width = 64;
+    this.height = 64;
+    this.movespeed = 12;
+    this.spriteSheetY = 0;
+    this.spriteSheetLocations = [0, 64, 384, 448, 128, 256, 320, 192];
+    this.currFrame = 0;
+    this.animationFrames = [0, 64, 128, 192];
+    this.isMoving = false;
+    this.paintColour = paintCol;
+    this.imgNum = imgNum;
+}
+
+var tealBlock = new Player(56, 32, "#39ffd2", 0);
+var redBlock = new Player(56, 520, "#f96e6e", 1);
+var greenBlock = new Player(520, 32, "#34ff41", 2);
+var yellowBlock = new Player(520, 520, "#fcff00", 3);
+
 var numConnections = 0;
+var playerChars = {};
 
 io.on("connection", function (socket) {
     console.log("connection made with id: " + socket.id);
@@ -78,17 +41,18 @@ io.on("connection", function (socket) {
 
     if(numConnections <= 4) {
         if(numConnections === 1) {
-            playerChars[socket.id] = yoda;
-            io.sockets.to(socket.id).emit("getChar", yoda);
+            playerChars[socket.id] = tealBlock;
+            io.sockets.to(socket.id).emit("getChar", tealBlock);
+            // io.sockets.emit("startGame", playerChars);
         } else if(numConnections === 2) {
-            playerChars[socket.id] = luke;
-            io.sockets.to(socket.id).emit("getChar", luke);
+            playerChars[socket.id] = redBlock;
+            io.sockets.to(socket.id).emit("getChar", redBlock);
         } else if(numConnections === 3) {
-            playerChars[socket.id] = ironman;
-            io.sockets.to(socket.id).emit("getChar", ironman);
+            playerChars[socket.id] = greenBlock;
+            io.sockets.to(socket.id).emit("getChar", greenBlock);
         } else {
-            playerChars[socket.id] = cap;
-            io.sockets.to(socket.id).emit("getChar", cap);
+            playerChars[socket.id] = yellowBlock;
+            io.sockets.to(socket.id).emit("getChar", yellowBlock);
         }
     }
     if(numConnections === 4) {
@@ -102,5 +66,9 @@ io.on("connection", function (socket) {
     socket.on("moveMade", function(data) {
         playerChars[socket.id] = data;
         io.sockets.emit("updateCanvas", playerChars);
+    });
+
+    socket.on("gameOver", function() {
+        io.sockets.emit("endGame", playerChars);
     });
 });
